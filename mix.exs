48caae5c0e659307130280b58d7902e858dfd727
CI <ci@users.noreply.github.com>
2024-12-05 10:54:41 +0000

    chore: release version v1.29.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule WandererApp.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/wanderer-industries/wanderer"
-  @version "1.29.0"
+  @version "1.29.1"
 
   def project do
     [
