50380bafea556767f9863d242c5d7ab9a5a6b891
Gustav <Guarzo.Eve@gmail.com>
2025-01-08 17:22:15 -0500

    zoo ci

--- config/dev.exs
+++ config/dev.exs
@@ -78,6 +78,11 @@ config :phoenix, :plug_init_mode, :runtime
 # Include HEEx debug annotations as HTML comments in rendered markup
 config :phoenix_live_view, :debug_heex_annotations, true
 
+# config/dev.exs
+config :phoenix_ddos,
+  enabled: false
+
+
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
 
