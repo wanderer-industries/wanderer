ef44881f0619b9f30160c09635a3bddb7fdc991f
achichenkov <aleksei.chichenkov@telleqt.ai>
2025-01-14 14:40:06 +0300

    fix(Map): Add style of corners for windows. Add ability to reset widgets. A lot of refactoring

--- assets/js/hooks/Mapper/components/mapInterface/MapInterface.tsx
+++ assets/js/hooks/Mapper/components/mapInterface/MapInterface.tsx
@@ -1,63 +1,25 @@
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
-import { useMemo, useState } from 'react';
-import { SESSION_KEY } from '@/hooks/Mapper/constants.ts';
+import { useMemo } from 'react';
 import { WindowManager } from '@/hooks/Mapper/components/ui-kit/WindowManager';
-import { WindowProps } from '@/hooks/Mapper/components/ui-kit/WindowManager/types.ts';
-import { CURRENT_WINDOWS_VERSION, DEFAULT_WIDGETS } from '@/hooks/Mapper/components/mapInterface/constants.tsx';
+import { DEFAULT_WIDGETS } from '@/hooks/Mapper/components/mapInterface/constants.tsx';
 import { useMapRootState } from '@/hooks/Mapper/mapRootProvider';
 
-type WindowsLS = {
-  windows: WindowProps[];
-  version: number;
-};
-
-const saveWindowsToLS = (toSaveItems: WindowProps[]) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const out = toSaveItems.map(({ content, ...rest }) => rest);
-  localStorage.setItem(SESSION_KEY.windows, JSON.stringify({ version: CURRENT_WINDOWS_VERSION, windows: out }));
-};
-
-const restoreWindowsFromLS = (): WindowProps[] => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const raw = localStorage.getItem(SESSION_KEY.windows);
-  if (!raw) {
-    console.warn('No windows found in local storage!!');
-    return DEFAULT_WIDGETS;
-  }
-
-  const { version, windows } = JSON.parse(raw) as WindowsLS;
-  if (!version || CURRENT_WINDOWS_VERSION > version) {
-    return DEFAULT_WIDGETS;
-  }
-
-  // eslint-disable-next-line no-debugger
-  const out = (windows as Omit<WindowProps, 'content'>[])
-    .filter(x => DEFAULT_WIDGETS.find(def => def.id === x.id))
-    .map(x => {
-      const content = DEFAULT_WIDGETS.find(def => def.id === x.id)?.content;
-      return { ...x, content: content! };
-    });
-
-  return out;
-};
-
 export const MapInterface = () => {
-  const [items, setItems] = useState<WindowProps[]>(restoreWindowsFromLS);
-  const { windowsVisible } = useMapRootState();
+  // const [items, setItems] = useState<WindowProps[]>(restoreWindowsFromLS);
+  const { windowsSettings, updateWidgetSettings } = useMapRootState();
 
-  const itemsFiltered = useMemo(() => {
-    return items.filter(x => windowsVisible.some(j => x.id === j));
-  }, [items, windowsVisible]);
+  const items = useMemo(() => {
+    return windowsSettings.windows
+      .map(x => {
+        const content = DEFAULT_WIDGETS.find(y => y.id === x.id)?.content;
+        return {
+          ...x,
+          content: content!,
+        };
+      })
+      .filter(x => windowsSettings.visible.some(j => x.id === j));
+  }, [windowsSettings]);
 
-  return (
-    <WindowManager
-      windows={itemsFiltered}
-      dragSelector=".react-grid-dragHandleExample"
-      onChange={x => {
-        saveWindowsToLS(x);
-        setItems(x);
-      }}
-    />
-  );
+  return <WindowManager windows={items} dragSelector=".react-grid-dragHandleExample" onChange={updateWidgetSettings} />;
 };
---
+++ assets/js/hooks/Mapper/mapRootProvider/hooks/useStoreWidgets.ts
@@ -0,0 +1,118 @@
+import useLocalStorageState from 'use-local-storage-state';
+import {
+  CURRENT_WINDOWS_VERSION,
+  DEFAULT_WIDGETS,
+  STORED_VISIBLE_WIDGETS_DEFAULT,
+  WidgetsIds,
+  WINDOWS_LOCAL_STORE_KEY,
+} from '@/hooks/Mapper/components/mapInterface/constants.tsx';
+import { WindowProps } from '@/hooks/Mapper/components/ui-kit/WindowManager/types.ts';
+import { useCallback, useEffect, useRef } from 'react';
+import { SNAP_GAP } from '@/hooks/Mapper/components/ui-kit/WindowManager';
+
+export type StoredWindowProps = Omit<WindowProps, 'content'>;
+export type WindowStoreInfo = {
+  version: number;
+  windows: StoredWindowProps[];
+  visible: WidgetsIds[];
+};
+export type UpdateWidgetSettingsFunc = (widgets: WindowProps[]) => void;
+export type ToggleWidgetVisibility = (widgetId: WidgetsIds) => void;
+
+export const getDefaultWidgetProps = () => ({
+  version: CURRENT_WINDOWS_VERSION,
+  visible: STORED_VISIBLE_WIDGETS_DEFAULT,
+  windows: DEFAULT_WIDGETS,
+});
+
+export const useStoreWidgets = () => {
+  const [windowsSettings, setWindowsSettings] = useLocalStorageState<WindowStoreInfo>(WINDOWS_LOCAL_STORE_KEY, {
+    defaultValue: getDefaultWidgetProps(),
+  });
+
+  const ref = useRef({ windowsSettings, setWindowsSettings });
+  ref.current = { windowsSettings, setWindowsSettings };
+
+  const updateWidgetSettings: UpdateWidgetSettingsFunc = useCallback(newWindows => {
+    const { setWindowsSettings } = ref.current;
+
+    setWindowsSettings(({ version, visible /*, windows*/ }: WindowStoreInfo) => {
+      return {
+        version,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        windows: DEFAULT_WIDGETS.map(({ content, ...x }) => {
+          const windowProp = newWindows.find(j => j.id === x.id);
+          if (windowProp) {
+            return windowProp;
+          }
+
+          return x;
+        }),
+        visible,
+      };
+    });
+  }, []);
+
+  const toggleWidgetVisibility: ToggleWidgetVisibility = useCallback(widgetId => {
+    const { setWindowsSettings } = ref.current;
+
+    setWindowsSettings(({ visible, windows, ...x }) => {
+      const isCheckedPrev = visible.includes(widgetId);
+      if (!isCheckedPrev) {
+        const maxZIndex = Math.max(...windows.map(w => w.zIndex));
+        return {
+          ...x,
+          windows: windows.map(wnd => {
+            if (wnd.id === widgetId) {
+              return { ...wnd, position: { x: SNAP_GAP, y: SNAP_GAP }, zIndex: maxZIndex + 1 };
+            }
+
+            return wnd;
+          }),
+          visible: [...visible, widgetId],
+        };
+      }
+
+      return {
+        ...x,
+        windows,
+        visible: visible.filter(x => x !== widgetId),
+      };
+    });
+  }, []);
+
+  useEffect(() => {
+    const { setWindowsSettings } = ref.current;
+
+    const raw = localStorage.getItem(WINDOWS_LOCAL_STORE_KEY);
+    if (!raw) {
+      console.warn('No windows found in local storage!!');
+
+      setWindowsSettings(getDefaultWidgetProps());
+      return;
+    }
+
+    const { version, windows, visible } = JSON.parse(raw) as WindowStoreInfo;
+    if (!version || CURRENT_WINDOWS_VERSION > version) {
+      setWindowsSettings(getDefaultWidgetProps());
+    }
+
+    // eslint-disable-next-line no-debugger
+    const out = windows.filter(x => DEFAULT_WIDGETS.find(def => def.id === x.id));
+
+    setWindowsSettings({
+      version: CURRENT_WINDOWS_VERSION,
+      windows: out as WindowProps[],
+      visible,
+    });
+  }, []);
+
+  const resetWidgets = useCallback(() => ref.current.setWindowsSettings(getDefaultWidgetProps()), []);
+
+  return {
+    windowsSettings,
+    updateWidgetSettings,
+    toggleWidgetVisibility,
+    resetWidgets,
+  };
+};
