aff00a18b504e517d0e4424c1a24d07e5032d4df
Dmitry Popov <dmitriypopovsamara@gmail.com>
2025-03-05 18:32:48 +0100

    feat: added auto-refresh timeout for cloud new version updates

--- assets/js/hooks/newVersionUpdate.ts
+++ assets/js/hooks/newVersionUpdate.ts
@@ -1,4 +1,43 @@
+const countdown = (secondsCount: number) => {
+  let minutes, seconds;
+
+  const dateEnd = new Date().getTime() + secondsCount * 1000;
+
+  const timer = setInterval(calculate, 1000);
+
+  function calculate() {
+    const dateStartDefault = new Date();
+    const dateStart = new Date(
+      dateStartDefault.getUTCFullYear(),
+      dateStartDefault.getUTCMonth(),
+      dateStartDefault.getUTCDate(),
+      dateStartDefault.getUTCHours(),
+      dateStartDefault.getUTCMinutes(),
+      dateStartDefault.getUTCSeconds(),
+    );
+    let timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000);
+
+    if (timeRemaining >= 0) {
+      timeRemaining = timeRemaining % 86400;
+      timeRemaining = timeRemaining % 3600;
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining = timeRemaining % 60;
+      seconds = parseInt(timeRemaining);
+
+      document.getElementById('version-update-seconds').innerHTML = minutes * 60 + seconds;
+    } else {
+      return;
+    }
+  }
+};
+
 const LAST_VERSION_KEY = 'wandererLastVersion';
+
+const updateVerion = (newVersion: string) => {
+  localStorage.setItem(LAST_VERSION_KEY, newVersion);
+
+  window.location.reload();
+};
 
 export default {
   mounted() {
@@ -14,12 +53,7 @@ export default {
         el.classList.add('hex-brick--active');
       });
 
-      setTimeout(() => {
-        const lastVersion = hook.el.dataset.version;
-        localStorage.setItem(LAST_VERSION_KEY, lastVersion);
-
-        window.location.reload();
-      }, 2000);
+      updateVerion(hook.el.dataset.version);
     };
 
     refreshZone.addEventListener('click', handleUpdate);
@@ -33,12 +67,23 @@ export default {
   },
 
   updated() {
+    const hook = this;
     const activeVersion = this.getItem(LAST_VERSION_KEY);
-    const lastVersion = this.el.dataset.version;
+    const lastVersion = hook.el.dataset.version;
     if (activeVersion === lastVersion) {
       return;
     }
-    this.el.classList.remove('hidden');
+    const enabled = hook.el.dataset.enabled;
+    if (enabled === 'true') {
+      hook.el.classList.remove('hidden');
+      const autoRefreshTimeout = Math.floor(Math.random() * (150 - 75 + 1)) + 75;
+      countdown(autoRefreshTimeout);
+      setTimeout(() => {
+        updateVerion(hook.el.dataset.version);
+      }, autoRefreshTimeout * 1000);
+    } else {
+      updateVerion(hook.el.dataset.version);
+    }
   },
 
   getItem(key: string) {
