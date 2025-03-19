badbefbade27a8e6195d1fce355646af850e7988
Dmitry Popov <dmitriypopovsamara@gmail.com>
2025-03-19 15:30:07 +0400

    Revert "fix: cloak key error behavior (#288)" (#290)
    This reverts commit 9b5ea2f84b3d90ee7d38aba441f6d0b480304a59.

--- lib/wanderer_app/vault.ex
+++ lib/wanderer_app/vault.ex
@@ -3,15 +3,11 @@ defmodule WandererApp.Vault do
 
   @impl GenServer
   def init(config) do
-    cipher_key = decode_env!("CLOAK_KEY")
-
     config =
       Keyword.put(config, :ciphers,
         default: {
           Cloak.Ciphers.AES.GCM,
-          tag: "AES.GCM.V1",
-          key: cipher_key,
-          iv_length: 12
+          tag: "AES.GCM.V1", key: decode_env!("CLOAK_KEY"), iv_length: 12
         }
       )
 
@@ -19,9 +15,8 @@ defmodule WandererApp.Vault do
   end
 
   defp decode_env!(var) do
-    key = System.get_env(var)
-    if is_nil(key),
-      do: raise("No environment variable found for #{var}"),
-      else: Base.decode64!(key)
+    var
+    |> System.get_env("OtPJXGfKNyOMWI7TdpcWgOlyNtD9AGSfoAdvEuTQIno=")
+    |> Base.decode64!()
   end
 end
