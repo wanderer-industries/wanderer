1be4ec2b907dac2dccb2e7f3ffbcd69fe616b9d6
Dmitry Popov <dmitriypopovsamara@gmail.com>
2025-05-11 14:03:53 +0200

    feat(Core): Updated map active characters page

--- lib/wanderer_app_web/live/map/components/map_characters.ex
+++ lib/wanderer_app_web/live/map/components/map_characters.ex
@@ -17,24 +17,26 @@ defmodule WandererAppWeb.MapCharacters do
      |> handle_info_or_assign(assigns)}
   end
 
-  # attr(:groups, :any, required: true)
-  # attr(:character_settings, :any, required: true)
-
   @impl true
   def render(assigns) do
     ~H"""
     <div id={@id}>
-      <ul :for={group <- @groups} class="space-y-4 border-t border-b border-gray-200 py-4">
+      <ul :for={group <- @groups} class="border-t border-b border-gray-200 py-0">
         <li :for={character <- group.characters}>
           <div class="flex items-center justify-between w-full space-x-2 p-1 hover:bg-gray-900">
-            <.character_entry character={character} character_settings={@character_settings} />
+            <.character_entry character={character} />
             <button
+              :if={character.tracked}
               phx-click="untrack"
               phx-value-event-data={character.id}
-              class="btn btn-sm btn-icon"
+              class="btn btn-sm btn-icon py-1"
             >
               <.icon name="hero-eye-slash" class="h-5 w-5" /> Untrack
             </button>
+
+            <span :if={not character.tracked} class="text-white rounded-full px-2">
+              Viewer
+            </span>
           </div>
         </li>
       </ul>
@@ -43,31 +45,43 @@ defmodule WandererAppWeb.MapCharacters do
   end
 
   attr(:character, :any, required: true)
-  attr(:character_settings, :any, required: true)
 
   defp character_entry(assigns) do
     ~H"""
     <div class="flex items-center gap-3 text-sm w-[450px]">
-      <span
-        :if={is_tracked?(@character.id, @character_settings)}
-        class="text-green-500 rounded-full px-2 py-1"
-      >
-        Tracked
+      <div class="flex flex-col p-4 items-center gap-2 tooltip tooltip-top" data-tip="Active from">
+        <span class="text-green-500 rounded-full px-2 py-1 whitespace-nowrap">
+          <.local_time id={@character.id} at={@character.from} />
+        </span>
+      </div>
+
+      <div class="avatar">
+        <div class="rounded-md w-8 h-8">
+          <img src={member_icon_url(@character.eve_id)} alt={@character.name} />
+        </div>
+      </div>
+      <span class="whitespace-nowrap">{@character.name}</span>
+      <span :if={@character.alliance_ticker} class="whitespace-nowrap">
+        [{@character.alliance_ticker}]
       </span>
+      <span :if={@character.corporation_ticker} class="whitespace-nowrap">
+        [{@character.corporation_ticker}]
+      </span>
+
       <span :if={is_online?(@character.id)} class="text-green-500 rounded-full px-2 py-1">
         Online
       </span>
       <span :if={not is_online?(@character.id)} class="text-red-500 rounded-full px-2 py-1">
         Offline
       </span>
-      <div class="avatar">
-        <div class="rounded-md w-8 h-8">
-          <img src={member_icon_url(@character.eve_id)} alt={@character.name} />
-        </div>
-      </div>
-      <span>{@character.name}</span>
-      <span :if={@character.alliance_ticker}>[{@character.alliance_ticker}]</span>
-      <span :if={@character.corporation_ticker}>[{@character.corporation_ticker}]</span>
+
+      <span :if={@character.tracked} class="text-green-500 rounded-full px-2 py-1">
+        Tracked
+      </span>
+
+      <span :if={not @character.tracked} class="text-red-500 rounded-full px-2 py-1 whitespace-nowrap">
+        Not Tracked
+      </span>
     </div>
     """
   end
@@ -77,12 +91,6 @@ defmodule WandererAppWeb.MapCharacters do
     # notify_to(socket.assigns.notify_to, socket.assigns.event_name, map_slug)
 
     {:noreply, socket}
-  end
-
-  defp is_tracked?(character_id, character_settings) do
-    Enum.any?(character_settings, fn setting ->
-      setting.character_id == character_id && setting.tracked
-    end)
   end
 
   defp is_online?(character_id) do
--- lib/wanderer_app_web/presence.ex
+++ lib/wanderer_app_web/presence.ex
@@ -10,19 +10,39 @@ defmodule WandererAppWeb.Presence do
   end
 
   def handle_metas(map_id, %{joins: _joins, leaves: _leaves}, presences, state) do
-    presence_character_ids =
+    presence_data =
       presences
-      |> Enum.map(fn {character_id, _} -> character_id end)
+      |> Enum.map(fn {character_id, meta} ->
+        from =
+          meta
+          |> Enum.map(& &1.from)
+          |> Enum.sort(&(DateTime.compare(&1, &2) != :gt))
+          |> List.first()
+
+        any_tracked = Enum.any?(meta, fn %{tracked: tracked} -> tracked end)
+
+        %{character_id: character_id, tracked: any_tracked, from: from}
+      end)
+
+    presence_tracked_character_ids =
+      presence_data
+      |> Enum.filter(fn %{tracked: tracked} -> tracked end)
+      |> Enum.map(fn %{character_id: character_id} ->
+        character_id
+      end)
 
     WandererApp.Cache.insert("map_#{map_id}:presence_updated", true)
-    WandererApp.Cache.insert("map_#{map_id}:presence_character_ids", presence_character_ids)
+
+    WandererApp.Cache.insert(
+      "map_#{map_id}:presence_character_ids",
+      presence_tracked_character_ids
+    )
+
+    WandererApp.Cache.insert(
+      "map_#{map_id}:presence_data",
+      presence_data
+    )
 
     {:ok, state}
-  end
-
-  def presence_character_ids(map_id) do
-    map_id
-    |> list()
-    |> Enum.map(fn {character_id, _} -> character_id end)
   end
 end
