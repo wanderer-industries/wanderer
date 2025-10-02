fd7e19e4905dbd40a29bda1f884903cd47a03e64
Dmitry Popov <dmitriypopovsamara@gmail.com>
2025-10-02 13:38:46 +0200

    feat(Core): Added PWA web manifest

--- lib/wanderer_app_web.ex
+++ lib/wanderer_app_web.ex
@@ -17,7 +17,9 @@ defmodule WandererAppWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images icons favicon.ico robots.txt woff woff2 lottie)
+  def static_paths,
+    do:
+      ~w(assets fonts images icons favicon.ico site.webmanifest apple-touch-icon.png web-app-manifest-192x192.png web-app-manifest-512x512.png robots.txt woff woff2 lottie)
 
   def router do
     quote do
