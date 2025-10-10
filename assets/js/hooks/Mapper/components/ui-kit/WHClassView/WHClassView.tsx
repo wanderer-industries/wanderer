dc4d260c9bde30a3984fb5e104c9386fbed3f0e9
DanSylvest <cublakhan257@gmail.com>
2025-10-10 22:10:54 +0300

    fix(Map): Fixed problem with rendering dropdown classes in signatures

--- assets/js/hooks/Mapper/components/ui-kit/WHClassView/WHClassView.tsx
+++ assets/js/hooks/Mapper/components/ui-kit/WHClassView/WHClassView.tsx
@@ -45,40 +45,42 @@ export const WHClassView = ({
   const whClass = useMemo(() => WORMHOLES_ADDITIONAL_INFO[whData.dest], [whData.dest]);
   const whClassStyle = WORMHOLE_CLASS_STYLES[whClass?.wormholeClassID] ?? '';
 
+  const content = (
+    <div
+      className={clsx(classes.WHClassViewContent, { [classes.NoOffset]: noOffset }, 'wh-name select-none cursor-help')}
+    >
+      {!hideWhClassName && <span className={clsx({ [whClassStyle]: highlightName })}>{whClassName}</span>}
+      {!hideWhClass && whClass && (
+        <span className={clsx(classes.WHClassName, whClassStyle, classNameWh)}>
+          {useShortTitle ? whClass.shortTitle : whClass.shortName}
+        </span>
+      )}
+    </div>
+  );
+
+  if (hideTooltip) {
+    return <div className={clsx(classes.WHClassViewRoot, className)}>{content}</div>;
+  }
+
   return (
     <div className={clsx(classes.WHClassViewRoot, className)}>
-      {!hideTooltip && (
-        <WdTooltipWrapper
-          position={TooltipPosition.bottom}
-          content={
-            <div className="flex gap-3">
-              <div className="flex flex-col gap-1">
-                <InfoDrawer title="Total mass">{prepareMass(whData.total_mass)}</InfoDrawer>
-                <InfoDrawer title="Jump mass">{prepareMass(whData.max_mass_per_jump)}</InfoDrawer>
-              </div>
-              <div className="flex flex-col gap-1">
-                <InfoDrawer title="Lifetime">{whData.lifetime}h</InfoDrawer>
-                <InfoDrawer title="Mass regen">{prepareMass(whData.mass_regen)}</InfoDrawer>
-              </div>
+      <WdTooltipWrapper
+        position={TooltipPosition.bottom}
+        content={
+          <div className="flex gap-3">
+            <div className="flex flex-col gap-1">
+              <InfoDrawer title="Total mass">{prepareMass(whData.total_mass)}</InfoDrawer>
+              <InfoDrawer title="Jump mass">{prepareMass(whData.max_mass_per_jump)}</InfoDrawer>
             </div>
-          }
-        >
-          <div
-            className={clsx(
-              classes.WHClassViewContent,
-              { [classes.NoOffset]: noOffset },
-              'wh-name select-none cursor-help',
-            )}
-          >
-            {!hideWhClassName && <span className={clsx({ [whClassStyle]: highlightName })}>{whClassName}</span>}
-            {!hideWhClass && whClass && (
-              <span className={clsx(classes.WHClassName, whClassStyle, classNameWh)}>
-                {useShortTitle ? whClass.shortTitle : whClass.shortName}
-              </span>
-            )}
+            <div className="flex flex-col gap-1">
+              <InfoDrawer title="Lifetime">{whData.lifetime}h</InfoDrawer>
+              <InfoDrawer title="Mass regen">{prepareMass(whData.mass_regen)}</InfoDrawer>
+            </div>
           </div>
-        </WdTooltipWrapper>
-      )}
+        }
+      >
+        {content}
+      </WdTooltipWrapper>
     </div>
   );
 };
