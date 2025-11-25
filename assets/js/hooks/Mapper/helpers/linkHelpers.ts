344ee54018ef1791ab75c35d083c5c11471c59a2
DanSylvest <cublakhan257@gmail.com>
2025-11-25 23:28:54 +0300

    feat: Add zkb and eve who links for characters where it possibly was add

---
+++ assets/js/hooks/Mapper/helpers/linkHelpers.ts
@@ -0,0 +1,2 @@
+export const charZKBLink = (characterId: string) => `https://zkillboard.com/character/${characterId}/`;
+export const charEveWhoLink = (characterId: string) => `https://evewho.com/character/${characterId}`;
