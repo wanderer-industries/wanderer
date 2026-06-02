ddf34351c96abc1b088636659766caf30c408a52
Drew Bonasera <Drewsif@users.noreply.github.com>
2026-06-02 08:40:10 -0400

    Move the `DB_HOST` environment variable evaluation from compile-time configuration to runtime configuration for the development and test environments. The compile-time `dev` and `test` configurations now default the database hostname to `localhost`.
    - fix(config): Set default database hostname to `localhost` in `dev.exs` and `test.exs`
    - fix(config): Add `DB_HOST` environment variable override for dev and test environments to `runtime.exs`

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :wanderer_app, WandererApp.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: System.get_env("DB_HOST", "localhost"),
+  hostname: "localhost",
   database: "wanderer_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
