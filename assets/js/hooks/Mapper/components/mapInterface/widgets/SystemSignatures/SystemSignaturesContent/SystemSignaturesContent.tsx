6dd049acf0a851d68d23192b90a24d8eb317cc0e
szesciennageometria <>
2026-08-13 18:07:01 +0200

    more fixes

--- assets/js/hooks/Mapper/components/mapInterface/widgets/SystemSignatures/SystemSignaturesContent/SystemSignaturesContent.tsx
+++ assets/js/hooks/Mapper/components/mapInterface/widgets/SystemSignatures/SystemSignaturesContent/SystemSignaturesContent.tsx
@@ -36,6 +36,8 @@ import { getSignatureRowClass } from '../helpers/rowStyles';
 
 const renderColIcon = (sig: SystemSignature) => renderIcon(sig);
 
+const EMPTY_GLOWING_ROWS = new Map()
+
 interface SystemSignaturesContentProps {
   systemId: string;
   signatures: ExtendedSystemSignature[];
@@ -67,7 +69,7 @@ export const SystemSignaturesContent = ({
   selectable,
   onSelect,
   filterSignature,
-  glowingRows = new Map(),
+  //glowingRows = new Map(),
 }: SystemSignaturesContentProps) => {
   const [selectedSignatureForDialog, setSelectedSignatureForDialog] = useState<SystemSignature | null>(null);
   const [showSignatureSettings, setShowSignatureSettings] = useState(false);
@@ -229,9 +231,10 @@ export const SystemSignaturesContent = ({
       rowData as ExtendedSystemSignature,
       refVars.current.selectedSignatures || [],
       refVars.current.settings[SETTINGS_KEYS.COLOR_BY_TYPE] as boolean,
-      glowingRows,
+      //glowingRows,
+      EMPTY_GLOWING_ROWS,
     );
-  }, [glowingRows]);
+  }, [/*glowingRows*/ EMPTY_GLOWING_ROWS]);
 
   const handleSortSettings = useCallback((e: DataTableStateEvent) => {
     refVars.current.settingsSignaturesUpdate({
