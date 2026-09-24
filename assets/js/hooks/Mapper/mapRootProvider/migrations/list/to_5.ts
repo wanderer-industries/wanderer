3bb9086f4b303ec63bd49cfbfc29aa15143c9eb0
szesciennageometria <>
2026-09-24 19:51:26 +0200

    attempt on resol;ving secondary paste bug when pastinng after reload or few seconds later was coloring all pasted rows green again.
    also re-adjusted to_5. or should i put that stuff into to_6 now ?

--- assets/js/hooks/Mapper/mapRootProvider/migrations/list/to_5.ts
+++ assets/js/hooks/Mapper/mapRootProvider/migrations/list/to_5.ts
@@ -1,15 +1,21 @@
 import { SIGNATURES_GLOWINGROWS_TIMING } from '@/hooks/Mapper/constants/signatures.ts';
+import { DEFAULT_JUMP_PLANNER_SETTINGS } from '@/hooks/Mapper/mapRootProvider/constants.ts';
+import { MigrationStructure } from '@/hooks/Mapper/mapRootProvider/types.ts';
 
 export const to_5: MigrationStructure = {
   to: 5,
   up: (prev: any) => {
- const signatureSettings = prev?.signatures || {};
+    const signatureSettings = prev?.signatures || {};
 
     return {
       ...prev,
       signatures: {
         ...signatureSettings,
         glowingrows_timing: signatureSettings.glowingrows_timing ?? SIGNATURES_GLOWINGROWS_TIMING.GLOWDEFAULT,
+      },
+      jumpPlanner: {
+        ...DEFAULT_JUMP_PLANNER_SETTINGS,
+        ...prev?.jumpPlanner,
       },
     };
   },
